c4de702934c0eba56881b982a15a3f0aed2c7b38
Diego7269 <93410781+Diego7269@users.noreply.github.com>
2023-02-04 13:41:53 -0800

    Have you showered yet box

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             children: [
               AnteaterStatus(),
-              ShoweringPrompt(),
+              ShoweringPrompt(data: data),
               ShowerCalendar(),
             ],
           ),
--- lib/showering_prompt.dart
+++ lib/showering_prompt.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
 
 // Displaying a box with button that asks if you showered
+import 'logic.dart';
+
 class ShoweringPrompt extends StatelessWidget {
-  const ShoweringPrompt({Key? key}) : super(key: key);
+  final UserData data;
+  const ShoweringPrompt({Key? key, required this.data}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.amber,
+      child: Card(
+        color: Colors.blue,
+        child: SizedBox(
+          width: 300,
+          height: 100,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Have you showered today?'),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  data.setDidShowerToday();
+                },
+                child: Text("Yes"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
